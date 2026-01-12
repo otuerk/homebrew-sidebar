bf1dbb12d0f00f78d26d7e29897d6439c6888484
otuerk <tuerk.oliver@gmail.com>
2026-01-12 16:06:56 +0100

    Update Sidebar to 1.9.4 (Automatic)

--- Casks/sidebar.rb
+++ Casks/sidebar.rb
@@ -2,9 +2,9 @@ cask "sidebar" do
   version "1.9.4"
   sha256 "52638e295c3b405879940869f41e8cae345b95a641bb2125fbd5347a3f2eeaa1"
 
-  url "https://download.sidebarapp.net/Sidebar.dmg"
+  url "https://download.sidebarapp.net/Sidebar%201.9.4.dmg"
   name "Sidebar"
-  desc "Sidebar App"
+  desc "Sidebar - The most customizable Dock replacement for macOS"
   homepage "https://sidebarapp.net/"
 
   livecheck do
