d2232be402e78d5dae40149d889512d80c0e610b
Oliver Tuerk <tuerk.oliver@gmail.com>
2026-03-13 21:02:02 +0100

    Update Sidebar to 2.0.0

--- Casks/sidebar.rb
+++ Casks/sidebar.rb
@@ -1,6 +1,6 @@
 cask "sidebar" do
   version "2.0.0"
-  sha256 "d5340f876b4fd3179ea6bc710b0c2e9d4834c3a5b8cd44af8371197d151c4489"
+  sha256 "17fcda6e59b90490c7da48fc8ba3b0f97173d762fe1fc5991dbbabbe26118aaf"
 
   url "https://download.sidebarapp.net/Sidebar%202.0.0.dmg"
   name "Sidebar"
