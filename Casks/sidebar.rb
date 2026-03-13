4955791b5d0ae91427282ab5eade7a6b661dbf1a
Oliver Tuerk <tuerk.oliver@gmail.com>
2026-03-13 18:59:28 +0100

    Update Sidebar to 2.0.0

--- Casks/sidebar.rb
+++ Casks/sidebar.rb
@@ -1,8 +1,8 @@
 cask "sidebar" do
-  version "1.9.9"
-  sha256 "c4b149f6adb760354db24732483f3b3b456c61095bbf1ce1c5d53f30ec45b443"
+  version "2.0.0"
+  sha256 "d5340f876b4fd3179ea6bc710b0c2e9d4834c3a5b8cd44af8371197d151c4489"
 
-  url "https://download.sidebarapp.net/Sidebar%201.9.9.dmg"
+  url "https://download.sidebarapp.net/Sidebar%202.0.0.dmg"
   name "Sidebar"
   desc "Sidebar - The most customizable Dock replacement for macOS"
   homepage "https://sidebarapp.net/"
